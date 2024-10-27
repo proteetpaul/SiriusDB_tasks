508d76b9783c3dec35deab99014a76903fec2d06
Proteet Paul <proteetpaul@gmail.com>
2024-10-26 20:07:03 -0500

    Add iterations

--- src/client.cpp
+++ src/client.cpp
@@ -11,17 +11,24 @@ int main() {
     stub = std::move(std::make_unique<Task1::Stub>(channel_ptr));
     grpc::ClientContext ctx;
     Empty request;
-    auto start = std::chrono::system_clock::now();
-    auto reader = stub->Get(&ctx, request);
-    Data data;
-    int num_chunks = 0;
-    while (reader->Read(&data)) {
-        num_chunks++;
+    uint64_t total = 0ll;
+    int num_chunks;
+    for (int i=0; i<5; i++) {
+        auto start = std::chrono::system_clock::now();
+        auto reader = stub->Get(&ctx, request);
+        Data data;
+        num_chunks = 0;
+        while (reader->Read(&data)) {
+            num_chunks++;
+        }
+        reader->Finish();
+        auto end = std::chrono::system_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+        total += duration.count();
     }
-    reader->Finish();
-    auto end = std::chrono::system_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    
     uint64_t total_bytes = num_chunks * 1ll * 2048;
-    std::cout << "Time taken: " << duration.count()/1e3 << "\n";
-    std::cout << "Bytes read: " << total_bytes << "\nThroughput: " << ((float)total_bytes/1e3)/duration.count() << "\n";
+    float time_per_iter = total/5.0f;
+    std::cout << "Time taken: " << time_per_iter << "\n";
+    std::cout << "Bytes read: " << total_bytes << "\nThroughput: " << ((float)total_bytes/1e3)/time_per_iter << "\n";
 }
